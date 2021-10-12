1c7230ccd216f05b957f6612e696009f89ca07e8
aokitashipro <webmocha2@gmail.com>
2021-10-13 04:45:21 +0900

    first

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <di>hello react!!</di>;
+const App = () => (
+  <div>app</div>
+)
 
 ReactDOM.render(<App />, document.getElementById("root"));
