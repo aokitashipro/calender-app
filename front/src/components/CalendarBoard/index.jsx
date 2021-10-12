d1ef5b3d504720d8d9f47101af62ad186f08b0c1
aokitashipro <webmocha2@gmail.com>
2021-10-13 05:14:03 +0900

    likeGoogleCalendar

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { GridList } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 import CalendarElement from "../CalendarElement";
 import { createCalendar } from "../../services/calendar";
 import * as styles from "./style.css";
+
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const calendar = createCalendar();
 
@@ -11,9 +13,22 @@ const CalendarBoard = () => {
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map(d => (
+            <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+          ))}
         {calendar.map(c => (
           <li key={c.toISOString()}>
-            <CalendarElement>{c.format("D")}</CalendarElement>
+            <CalendarElement day={c} />
           </li>
         ))}
       </GridList>
